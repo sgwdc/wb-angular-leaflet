44391ba685854028773296f4e7597a0016f013c5
Steven Greenwaters <steven.greenwaters@gmail.com>
2019-11-05 15:01:11 -0500

    Add WMS layer options suggested by World Bank

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,14 +16,30 @@ export class AppComponent {
     zoom: 17,
     center: latLng(38.898947, -77.042478)
     */
-    // Continental U.S.
+    /* Continental U.S.
     zoom: 4,
     center: latLng(39.8333333,-98.585522)
+    */
+    // Earth
+    zoom: 2,
+    center: latLng(30, 0)
   };
   layersControl = {
     baseLayers: {
       'Open Street Map': tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' }),
       'Open Cycle Map': tileLayer('https://tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey=081af2bf069048f9ac676796465d77bf', { maxZoom: 18, attribution: '...' })
     },
+    overlays: {
+      'GPWv4: Population Density - 2015': tileLayer.wms('https://sedac.ciesin.columbia.edu/geoserver/wms', {
+        layers: 'gpw-v4:gpw-v4-population-density_2015',
+        format: 'image/png',
+        transparent: true,
+      }),
+      'Probabilities of Urban Expansion to 2030': tileLayer.wms('https://sedac.ciesin.columbia.edu/geoserver/wms', {
+        layers: 'lulc:lulc-global-grid-prob-urban-expansion-2030',
+        format: 'image/png',
+        transparent: true,
+      }),
+    }
   };
 }
