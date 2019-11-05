845d46bcc35feb7f8b2b2c0b885425f10f1344ad
Steven Greenwaters <steven.greenwaters@gmail.com>
2019-11-05 14:07:25 -0500

    Add layers control with basemap options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,18 @@ export class AppComponent {
     layers: [
       tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
     ],
+    /* World Bank HQ
     zoom: 17,
     center: latLng(38.898947, -77.042478)
+    */
+    // Continental U.S.
+    zoom: 4,
+    center: latLng(39.8333333,-98.585522)
+  };
+  layersControl = {
+    baseLayers: {
+      'Open Street Map': tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' }),
+      'Open Cycle Map': tileLayer('https://tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey=081af2bf069048f9ac676796465d77bf', { maxZoom: 18, attribution: '...' })
+    },
   };
 }
